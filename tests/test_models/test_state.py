2bc66b25fd08bfbd37d0db1ab5978a37eeffcc08
devbn3li <fudwhc@gmail.com>
2023-10-15 19:36:19 +0300

    push test_City

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unittest for State
+"""
+
+import unittest
+from models.state import State
+
+
+class Test_State(unittest.TestCase):
+    """Class to test the State class
+    """
+
+    def test_attrib(self):
+        """
+        Test the attributes of the State class
+        """
+
+        my_state = State()
+        
+        self.assertTrue(my_state.name, '')
