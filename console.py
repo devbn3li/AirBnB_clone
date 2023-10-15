a43ec5df082dd200b8d128b786dd0110fd7fc22d
devbn3li <fudwhc@gmail.com>
2023-10-15 23:27:54 +0300

    console.py

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+command interpreter using cmd module
+"""
+
+
+import cmd
+import sys
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.state import State
+from models.city import City
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """class cmd"""
+
+    prompt = '(hbnb) '
+    classes = ['BaseModel', 'User', 'Place', 'State', 'City', 'Amenity',
+               'Review']
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
