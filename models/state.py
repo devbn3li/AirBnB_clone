23f5a2b7e85f704e39dc5ca71a73d954def43ad1
devbn3li <fudwhc@gmail.com>
2023-10-15 19:10:25 +0300

    push

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to get BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class State that inherits from BaseModel"""
+    
+    name = ""
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Unittest for BaseModel
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """unittest_BaseModel"""
+
+    def test_type(self):
+        """test type"""
+
+        object = BaseModel()
+        object.name = "ALX model"
+        object.my_number = 89
+        self.assertEqual(object.name, "ALX model")
+        self.assertEqual(object.my_number, 89)
