151f4608c0d1a1b8565328704cbe5ad9ea95906b
devbn3li <fudwhc@gmail.com>
2023-10-16 16:52:02 +0300

    test_user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unittest for User"""
+
+import unittest
+from models.user import User
+
+
+class Test_User(unittest.TestCase):
+    """unittest_User"""
+
+    def test_attrib(self):
+        """test for User attributes"""
+
+        model = User()
+
+        self.assertEqual(model.email, '')
+        self.assertEqual(model.password, '')
+        self.assertEqual(model.first_name, '')
+        self.assertEqual(model.last_name, '')
+
+if __name__ == '__main__':
+    unittest.main()
